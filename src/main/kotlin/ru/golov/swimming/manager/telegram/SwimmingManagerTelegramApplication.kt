1b05f49e511c2600b18d8d0f4c9354f224b6b25b
GolovPavel <golovpasha@gmail.com>
2020-10-10 18:51:31 +0300

    Init kotlin project

---
+++ src/main/kotlin/ru/golov/swimming/manager/telegram/SwimmingManagerTelegramApplication.kt
@@ -0,0 +1,11 @@
+package ru.golov.swimming.manager.telegram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwimmingManagerTelegramApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwimmingManagerTelegramApplication>(*args)
+}
---
+++ src/test/kotlin/ru/golov/swimming/manager/telegram/SwimmingManagerTelegramApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.golov.swimming.manager.telegram
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwimmingManagerTelegramApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
