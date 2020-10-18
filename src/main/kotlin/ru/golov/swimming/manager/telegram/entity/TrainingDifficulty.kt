b51c1e1a8a01e55eb71c85bca2f72f68607814d7
GolovPavel <golovpasha@gmail.com>
2020-10-18 18:04:51 +0300

    Refactoring

---
+++ src/main/kotlin/ru/golov/swimming/manager/telegram/entity/TrainingDifficulty.kt
@@ -0,0 +1,8 @@
+package ru.golov.swimming.manager.telegram.entity
+
+enum class TrainingDifficulty {
+
+    LOW,
+    MEDIUM,
+    HARD
+}
--- src/main/kotlin/ru/golov/swimming/manager/telegram/entity/TrainingProperties.kt
+++ src/main/kotlin/ru/golov/swimming/manager/telegram/entity/TrainingProperties.kt
@@ -1,10 +1,13 @@
 package ru.golov.swimming.manager.telegram.entity
 
 import java.time.DayOfWeek
+import javax.persistence.CollectionTable
 import javax.persistence.Column
 import javax.persistence.ElementCollection
 import javax.persistence.Embeddable
+import javax.persistence.EnumType
 import javax.persistence.Enumerated
+import javax.persistence.JoinColumn
 
 @Embeddable
 class TrainingProperties(
@@ -13,10 +16,12 @@ class TrainingProperties(
         val trainingDistance: Int,
 
         @Column(name = "training_difficulty")
-        val trainingDifficulty: Int,
+        @Enumerated(EnumType.STRING)
+        val trainingDifficulty: TrainingDifficulty,
 
-        @Column
-        @Enumerated
+        @Enumerated(EnumType.STRING)
+        @Column(name = "day_of_the_week")
         @ElementCollection(targetClass = DayOfWeek::class)
+        @CollectionTable(name = "user_training_days", joinColumns = [JoinColumn(name = "user_info_id")])
         val trainingDays: List<DayOfWeek> = mutableListOf()
 )
